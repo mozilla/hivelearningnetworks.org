e329316f9b9c154c35a3c444924fca3d54cd1f5c
Mavis Ou <mmmavisss@gmail.com>
2014-06-13 14:31:15 -0700

    Add the 'Coming Soon' page

---
+++ themes/hive-learning-networks/page-coming-soon.php
@@ -0,0 +1,29 @@
+  <?php
+  /*
+  Template Name: "Coming Soon" Page
+  */
+
+  get_header(); ?>
+
+    <div>
+      <div class="container text-center">
+        <div class="row">
+          <div class="col-md-12">
+            <div>
+              <img src="<?php echo get_template_directory_uri(); ?>/img/hive_logo_coloured.png" class="logo" />
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-12 coming-soon-msg">
+            <h1>New site coming soon in July 2014</h1>
+            <h2>Visit <a href="http://hivelearningnetwork.org/">hivelearningnetwork.org</a> for more info</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+
+</div> <!-- closing #wrap -->
+
+
+<?php get_footer(); ?>
