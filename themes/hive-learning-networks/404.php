4201799a2d3486b72dcc0c790fab57d2ba0c64d0
Mavis Ou <mmmavisss@gmail.com>
2016-04-21 12:40:07 -0700

    added a super simple 404 page

---
+++ themes/hive-learning-networks/404.php
@@ -0,0 +1,22 @@
+  <?php
+  /*
+  Template Name: "404" page
+  */
+
+  get_header(); ?>
+
+    <div id="page-404">
+      <div class="container">
+        <div class="row">
+          <div id="main" class="col-md-12">
+            <h1>Oops!</h1>
+            <h2>Page not found</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </div> <!-- closing #wrap -->
+
+    <?php get_footer(); ?>
+
